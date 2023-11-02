36b444a02e8cb7f4cf6fb3b3761a57241230eb9c
Saransh Vashisht <saranshs557@gmail.com>
2023-11-02 08:50:25 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# Student Enrollment Form
+
+## Description
+
+This is a web-based HTML form for student enrollment using JsonPowerDB as the database. JsonPowerDB is a real-time, high-performance, lightweight, and simple-to-use REST API-based multi-mode DBMS. It has ready-to-use APIs for JSON document DB, RDBMS, key-value DB, geospatial DB, and time series DB functionality. JPDB supports and advocates for true serverless and pluggable API development.
+
+This project allows users to perform CRUD operations on student enrollment data. CRUD stands for create, read, update, and delete. Users can create new student records, read existing records, update records, and delete records.
+
+This project is useful for educational institutions that want to manage their student enrollment data in a more efficient and effective way. It can also be used for other purposes, such as online examination registration, job application, and profile creation.
+
+## Benefits of Using JsonPowerDB
+
+The benefits of using JsonPowerDB for this project include:
+
+- Simplest way to retrieve data in a JSON format.
+- Schema-free, simple to use, nimble, and in-memory database.
+- Built on top of one of the fastest and real-time data indexing engines - PowerIndeX.
+- Low level (raw) form of data and is also human readable.
+- Helps developers in faster coding, in-turn reduces development cost.
+
+## CRUD operations
+
+- Create operation allows users to create new student records. This can be done by entering the student's name, address, phone number, and other relevant information into the form.
+
+- Read operation allows users to view existing student records. This can be done by searching for the student's name, address, or other identifying information.
+
+- Update operation allows users to modify existing student records. This can be done by entering the new information into the form and clicking the "Update" button.
+
+- Delete operation allows users to remove existing student records. This can be done by selecting the student's name from the list and clicking the "Delete" button.
+
+## Tech Stack Used 
+
+- Front-end : HTML, CSS , JAVASCRIPT
+- Database : JsonPowerDB
+
+## Screenshots
+
+
+
+
---
+++ index.js
@@ -0,0 +1,203 @@
+var jpdbBaseURL = "http://api.login2explore.com:5577";
+var jpdbIRL = "/api/irl";
+var jpdbIML = "/api/iml";
+var stdDBName = "SCHOOL-DB";
+var stdRelationName = "STUDENT-TABLE";
+var connToken = "90931539|-31949330549365523|90959905";
+$("#stdRoll").focus();
+
+function saveRecNo2LS(jsonObj) {
+  var lvData = JSON.parse(jsonObj.data);
+  localStorage.setItem("rec_no", lvData.rec_no);
+}
+
+function getStdRollAsJsonObj() {
+  var stdRoll = $("#stdRoll").val();
+  var jsonStr = {
+    rollno: stdRoll,
+  };
+  return JSON.stringify(jsonStr);
+}
+
+function disableAllFeildExceptRollno() {
+  $('#stdName').prop('disabled', true);
+  $('#stdClass').prop('disabled', true);
+  $('#stdbirthDate').prop('disabled', true);
+  $('#stdAddress').prop('disabled', true);
+  $('#stdEnroll').prop('disabled', true);
+  $('#reset').prop('disabled', true);
+  $('#save').prop('disabled', true);
+  $('#update').prop('disabled', true);
+}
+
+
+function fillData(jsonObj) {
+  saveRecNo2LS(jsonObj);
+  var record = JSON.parse(jsonObj.data).record;
+  $("#stdName").val(record.name);
+  $("#stdClass").val(record.class);
+  $("#stdBirthdate").val(record.birthdate);
+  $("#stdAddress").val(record.address);
+  $("#stdEnroll").val(record.enrollment_date);
+}
+function validateData() {
+  var stdRoll, stdName, stdClass, stdBirthdate, stdAddress, stdEnroll;
+  stdRoll = $("#stdRoll").val();
+  stdName = $("#stdName").val();
+  stdClass = $("#stdClass").val();
+  stdBirthdate = $("#stdBirthdate").val();
+  stdAddress = $("#stdAddress").val();
+  stdEnroll = $("#stdEnroll").val();
+  if (stdRoll === "") {
+    alert("Roll Number is missing");
+    $("#stdRoll").focus();
+    return "";
+  }
+  if (stdRoll <= 0) {
+   alert("Invalid Roll Number");
+    $('#stdRoll').focus();
+    return "";
+}
+  if (stdName === "") {
+    alert("Student name is missing");
+    $("#stdName").focus();
+    return "";
+  }
+  if (stdClass === "") {
+    alert("Student Class is missing");
+    $("#stdClass").focus();
+    return "";
+  }
+  if (stdBirthdate === "") {
+    alert("Student Birthdate is missing");
+    $("#stdBirthdate").focus();
+    return "";
+  }
+  if (stdAddress === "") {
+    alert("Student Address is missing");
+    $("#stdAddress").focus();
+    return "";
+  }
+  if (stdEnroll === "") {
+    alert("Student Enrollement Date  is missing");
+    $("#stdEnroll").focus();
+    return "";
+  }
+  var jsonStrObj = {
+    rollno: stdRoll,
+    name: stdName,
+    class: stdClass,
+    birthdate: stdBirthdate,
+    address: stdAddress,
+    enrollment_date: stdEnroll,
+  };
+  return JSON.stringify(jsonStrObj);
+}
+
+function resetForm() {
+  $("#stdRoll").val("");
+  $("#stdName").val("");
+  $("#stdClass").val("");
+  $("#stdBirthdate").val("");
+  $("#stdAddress").val("");
+  $("#stdAddress").val("");
+  $("#stdEnroll").val("");
+  $("#stdRoll").prop("disabled", false);
+  disableAllFeildExceptRollno();
+  $("#empId").focus();
+}
+
+function getStd() {
+  if ($('#stdRoll').val() === "") { // if roll number is not given then disable all feild
+    disableAllFeildExceptRollno();
+} else if ($('#stdRoll').val() < 1) { // if roll number is not valid (i.e roll-no <1)
+    disableAllFeildExceptRollno();
+    alert("Invalid Roll Number");
+    $('#stdRoll').focus;
+}else{
+  var stdRollJsonObj = getStdRollAsJsonObj();
+  var getRequest = createGET_BY_KEYRequest(
+    connToken,
+    stdDBName,
+    stdRelationName,
+    stdRollJsonObj
+  );
+  jQuery.ajaxSetup({ async: false });
+  var resJsonObj = executeCommandAtGivenBaseUrl(
+    getRequest,
+    jpdbBaseURL,
+    jpdbIRL
+  );
+  jQuery.ajaxSetup({ async: true });
+  //Enable all Fields
+  $('#stdRoll').prop('disabled', false);
+  $('#stdName').prop('disabled', false);
+  $('#stdClass').prop('disabled', false);
+  $('#stdBirthdate').prop('disabled', false);
+  $('#stdAddress').prop('disabled', false);
+  $('#stdEnroll').prop('disabled', false);
+
+
+  if (resJsonObj.status === 400) {
+    $("#save").prop("disabled", false);
+    $("#reset").prop("disabled", false);
+    $("#stdName").focus();
+  } else if (resJsonObj.status === 200) {
+    $("#stdRoll").prop("disabled", true);
+    fillData(resJsonObj);
+    $("#update").prop("disabled", false);
+    $("#reset").prop("disabled", false);
+    $("#stdName").focus();
+  }
+}
+}
+
+function saveData() {
+  var jsonStrObj = validateData();
+  if (jsonStrObj === "") {
+    return "";
+  }
+  var putRequest = createPUTRequest(
+    connToken,
+    jsonStrObj,
+    stdDBName,
+    stdRelationName
+  );
+  jQuery.ajaxSetup({ async: false });
+  var resJsonObj = executeCommandAtGivenBaseUrl(
+    putRequest,
+    jpdbBaseURL,
+    jpdbIML
+  );
+
+  jQuery.ajaxSetup({ async: true });
+  console.log(resJsonObj);
+  resetForm();
+  $("#stdRoll").focus();
+}
+
+function updateData() {
+  $("#update").prop("disabled", true);
+  var jsonStrObj = validateData();
+  if (jsonStrObj == "") {
+    return;
+  }
+  alert(jsonStrObj);
+  var updateRequest = createUPDATERecordRequest(
+    connToken,
+    jsonStrObj,
+    stdDBName,
+    stdRelationName,
+    localStorage.getItem("rec_no")
+  );
+  jQuery.ajaxSetup({ async: false });
+  var resJsonObj = executeCommandAtGivenBaseUrl(
+    updateRequest,
+    jpdbBaseURL,
+    jpdbIML
+  );
+  jQuery.ajaxSetup({ async: true });
+  console.log(resJsonObj);
+  resetForm();
+  $("#stdRoll").focus();
+}
